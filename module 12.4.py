46c238fab544dc26b39ae384639183713bc9e6c7
Magomeds05 <yusypovm@gmail.com>
2024-10-01 17:48:51 +0300

    Update module 12.4.py

--- module 12.4.py
+++ module 12.4.py
@@ -1,29 +1,31 @@
-import rt_with_exceptions
-from tests_12_3 import RunnerTest
-import runner
+from with_exception import Runner, Tournament
 import logging
 import unittest
 
-logging.basicConfig(level=logging.INFO, filemode="w", filename='runner_tests.log',
+logging.basicConfig(level=logging.INFO, filemode="w", filename='runner_tests2.log',
                     encoding='utf-8', format="%(levelname)s ! %(message)s")
 
 class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
         try:
-            x = runner.Runner('Mark')
-            logging.info(f'{test_walk} выполнен успешно')
+            x = Runner('Mark', -5)
             for i in range(10):
                x.walk()
             self.assertEqual(x.distance, 50, f'{x.name} должен пройти 50 метров, а прошел {x.distance}')
-        except:
-            logging.error(f'Неверная скорость для{Runner}')
+            logging.info(f'"test_walk" выполнен успешно')
+        except ValueError as exc:
+            logging.error(f'Неверная скорость для{Runner}', exc_info=exc)
 
 
     def test_run(self):
-        y = runner.Runner('Ugor')
-        for i in range(10):
-            y.run()
-        self.assertEqual(y.distance, 100, f'{y.name} должен пробежать 100 метров, а пробежал {y.distance}')
+        try:
+            y = Runner(40)
+            for i in range(10):
+               y.run()
+            self.assertEqual(y.distance, 100, f'{y.name} должен пробежать 100 метров, а пробежал {y.distance}')
+            logging.info(f'"test_run" выполнен успешно')
+        except TypeError as exc:
+            logging.error(f'Неверный тип данных для объекта {Runner}', exc_info=exc)
 
 
